2dc20eee54bba6474b959831d29012c4c96f867c
Avarjana <acpasavarjana@gmail.com>
2020-04-03 03:55:07 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# debugger
+
+You can play this until 4th april, 11.59 PM. Time will be recorded. 
+
+This is a flutter project.
+
+The code contains bugs and aliens maybe. There are hints in the code.
+Make sure you look carefully. What we ignored or laughed at might be important.
+
+You will find this useful -> https://www.facebook.com/itfsu/ (not now but later)
+
+You can use a Postman (https://www.postman.com/) like application to try the http requests.
+Dio (https://pub.dev/packages/dio) library is added to the packages for http requests.
+Tip from the creator: Use Postman. 
+
+(Requests per 5 minutes is limited to 2. You have to wait 5 minutes if you send a wrong body twice)
+
+Make sure to pull if there are new changes. That might save you trouble.
+
+Good Luck 😁
+
+One more thing. You either get 100 marks or 0 marks for this task. You can't just "debug partially" and be happy right? 😅
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,152 @@
+import 'package:dio/dio.dart';
+import 'package:iraj/productions.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  public static void main(String args[]){
+    System.out.println("You will find hints all over the code");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  
+  final static String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String username = '';
+  String password = '';
+  String might;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        color: Color.fromARGB(255, 24, 144, 255),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(
+                'assets/icon.png',
+                width: MediaQuery.of(context).size.width / 2,
+              ),
+              Container(
+                colour: Colour.fromARGB(255, 63, 81, 181),
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                child: Text(
+                  'Welcome',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(LOL).size.width / 1.5,
+                margin: EdgeInsets.only(top: 10, bottom: 20),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Username',
+                  ),
+                  onChanged: (text) {
+                    username = text;
+                  },
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width / 1.5,
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Color.fromARGB(255, 255, 255, 255),
+                    filled: true,
+                    hintText: 'Password',
+                  ),
+                  onChanged: (text) {
+                    password = text;
+                  },
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: MaterialButton(
+                  child: Text('Submit'),
+                  color: Color.fromARGB(255, 63, 81, 181),
+                  textColor: Colors.white,
+                  onPressed: () {
+                    passwordSubmit();
+                    /**
+                     * Oops... It's encypted in some way. 
+                     * What's encrypted? Well find it yourself.
+                     * Be smart. You will find how to.
+                     * 
+                     * "value" is,
+                     * U2FsdGVkX19x0A+YNgfKIZQkNbgeigOyN8bdXgkCK+M5JPf7TCAdC+YBPheFBcPtVgGXCkXV/GeziqNYDsJPpHgxKqli6rbxDEYe+GH4vkVZUkP+991qSsusSvUovEZWoPBwukpUiDhFfNT6wAzBsYPwjT5oFBs41Gfdiq48CeBZZK+20qOltQOKnJxXb7TkMc6MykUPUrStzTEXDO0I7fmAxzkpiX4aHrHUlvPgHjVaXrOXXo34ccp5lT/Q4MCa
+                     */
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void passwordSubmit() async {
+    // once you run the app, username and password will come to you. Not by code but with your eyes and memory.
+    // try to match and you'll see :D
+    try {
+      Response response = await Dio().post("some_url",data: {});
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void removeCOVID19() {
+    print("Covid-19 Removed :) ");
+  }
+
+  void dummyFunction() {
+    this.might = "help to decrypt";
+    //(Only 2 requests per 5 minutes are allowed)
+    Dio().post("https://debugger.homealone2020.live/decrypt",data: {
+      "value":"",
+      "code":""
+    });
+    /*
+    But what is the "code"??? hmm.... You have to find that!
+    Try looking in the place where we put our packages.
+    */
+  }
+
+  TextFormField homeAlone2020() {
+    return TextFormField(
+      decoration: new InputDecoration(
+        labelText: 'a',
+      ),
+      validator: (value) => value.isEmpty ? "he he" : null,
+    );
+  }
+
+}
